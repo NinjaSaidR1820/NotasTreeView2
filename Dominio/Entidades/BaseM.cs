0e3f969b1c3b50b11d48ab36724b86ff1da39a6c
Leandro-Kun <90274325+Leandro-Ramirez@users.noreply.github.com>
2022-03-16 08:58:01 -0600

    Block De Notas Version 2.0
    Falta Instalar el AutoFac desde Herramientas (Tools), nugets, etc

---
+++ Dominio/Entidades/BaseM.cs
@@ -0,0 +1,15 @@
+﻿#region Usos
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+#endregion
+
+namespace Dominio.Entidades
+{
+    public interface BaseM : Modelo1<String>
+    {
+        String Guardar(string text);
+    }
+}
---
+++ Infrastructura/Acciones/Directo.cs
@@ -0,0 +1,58 @@
+﻿#region Usos
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Dominio.Entidades;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+#endregion
+
+namespace Infrastructura.Acciones
+{
+    public class Directo : BaseM
+    {
+        private BinaryWriter binaryWriter;
+
+        public Directo()
+        {
+
+        }
+        public void Add(string texto, string ruta)
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream(ruta, FileMode.Append, FileAccess.Write))
+                {
+                    binaryWriter = new BinaryWriter(fileStream);
+                    binaryWriter.Write(texto);
+                }
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+        }
+
+        public string Guardar(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Read(string t)
+        {
+            string text = string.Empty;
+
+            try
+            {
+                text = File.ReadAllText(t);
+
+                return text;
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+        }
+    }
+}
